fd5395f2039d8b6f80f0b97c1c0a3b2ced69a1c6
ishvar99 <ishanvarshney99@gmail.com>
2020-07-27 17:31:33 +0530

    working on reset password functionality

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const userSchema = new mongoose.Schema(
   {
     name: {
