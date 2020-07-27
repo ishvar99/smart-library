b7eb746fb429d3b4d6a42a3a03fd3eb0ad7974e9
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-27 16:41:16 +0530

    login css and few fixes

--- frontend/src/components/basic/Footer/Footer.js
+++ frontend/src/components/basic/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export const Footer = () => {
-  return <div></div>
+  return <div>Hi</div>
 }
--- frontend/src/components/basic/Routing.js
+++ frontend/src/components/basic/Routing.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
-import { Home } from "../pages/Home"
-import { Login } from "../pages/Login"
-import { Register } from "../pages/Register"
+import { Home } from "../pages/Home/Home"
+import { Login } from "../pages/Login/Login"
+import { Register } from "../pages/Register/Register"
 
 export const Routing = () => {
   return (
