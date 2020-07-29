9ad1442eb3516b482163b30eb1d90d42957d0376
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-29 05:57:28 +0530

    home commponents - carousel and trending basic theming

---
+++ frontend/src/components/pages/Home/components/Trending.js
@@ -0,0 +1,28 @@
+import React from "react"
+import "./trending.scss"
+
+export const Trending = ({ data }) => {
+  return (
+    <div className='Trending'>
+      <div className='section-title'>
+        <h4>Treadings</h4>
+        <h6>Explore</h6>
+      </div>
+      <div className='main-wrapper'>
+        {data.map((item, index) => {
+          return (
+            <div key={index} className='book-wrapper'>
+              <img
+                className='book-img'
+                src={item.image}
+                alt='book-cover'
+                height='200'
+                width='150'
+              />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
