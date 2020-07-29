6be2ef5b638aa849b22909d1c379c1c98c677862
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-29 15:09:47 +0530

    isAuthenticated added in protect.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const { getUserByID, getUser, updateUser } = require("../controllers/user")
-const { isLoggedin } = require("../middlewares/protect")
+const { isLoggedin, isAuthenticated } = require("../middlewares/protect")
 const router = express.Router()
 
 router.param("userID", getUserByID)
-router.get("/user/:userID", isLoggedin, getUser)
-router.put("/user/:userID", isLoggedin, updateUser)
+router.get("/user/:userID", isLoggedin, isAuthenticated, getUser)
+router.put("/user/:userID", isLoggedin, isAuthenticated, updateUser)
 
 module.exports = router
