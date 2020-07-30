e70109e082c70566757eb6718d4de83cfa56d4fb
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-07-30 23:54:44 +0530

    profile edit added

--- frontend/src/components/pages/Profile/components/PersonalInformation.js
+++ frontend/src/components/pages/Profile/components/PersonalInformation.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { Card, Form, Button, Col } from "react-bootstrap"
 
 export const PersonalInformation = () => {
+  const [edit, setEdit] = useState(false)
   const [validated, setValidated] = useState(false)
 
   const handleSubmit = (event) => {
@@ -13,12 +14,22 @@ export const PersonalInformation = () => {
 
     setValidated(true)
   }
+
+  const handleEdit = (e) => {
+    setEdit(!edit)
+  }
   return (
     <div className='tabContent' id='PersonalInformation'>
       <Card className='p-3'>
         <Card.Title>
           Personal Information
-          <span className='edit-text text-primary float-right'> Edit</span>
+          <Button
+            type='button'
+            className=' btn btn-sm btn-light bg-white text-primary float-right'
+            onClick={handleEdit}
+          >
+            {!edit ? <span>Edit</span> : <span> Cancel</span>}
+          </Button>
         </Card.Title>
         <Card.Body>
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
@@ -26,14 +37,14 @@ export const PersonalInformation = () => {
               <Form.Row>
                 <Form.Group as={Col} md={5}>
                   <Form.Label>Name</Form.Label>
-                  <Form.Control required type='text' disabled />
+                  <Form.Control required type='text' disabled={!edit} />
                   <Form.Control.Feedback type='invalid'>
                     Please enter your name
                   </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group as={Col} md={2}>
                   <Form.Label>Age</Form.Label>
-                  <Form.Control required type='number' disabled />
+                  <Form.Control required type='number' disabled={!edit} />
                   <Form.Control.Feedback type='invalid'>
                     Age is required
                   </Form.Control.Feedback>
@@ -43,20 +54,20 @@ export const PersonalInformation = () => {
 
             <Form.Group>
               <Form.Label>Bio</Form.Label>
-              <Form.Control as='textarea' rows='3' disabled />
+              <Form.Control as='textarea' rows='3' disabled={!edit} />
             </Form.Group>
 
             <Form.Row>
               <Form.Group as={Col}>
                 <Form.Label>Email address</Form.Label>
-                <Form.Control required disabled type='email' />
+                <Form.Control required disabled={!edit} type='email' />
                 <Form.Control.Feedback type='invalid'>
                   Enter a valid email address
                 </Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
 
-            <Button variant='primary' type='submit'>
+            <Button disabled={!edit} variant='primary' type='submit'>
               Submit
             </Button>
           </Form>
