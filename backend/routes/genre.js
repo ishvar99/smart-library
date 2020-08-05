f65db3e0077548a6a88133f34ee38daa99814d9e
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-08-05 07:49:03 +0530

    createGenre and getAllgenres added

--- backend/routes/genre.js
+++ backend/routes/genre.js
@@ -1,4 +1,32 @@
 const express = require("express")
+const { getUserByID } = require("../controllers/user")
+const {
+  getGenreByID,
+  createGenre,
+  getAllGenres,
+} = require("../controllers/genre")
+const { route } = require("./auth")
+const {
+  isLoggedin,
+  isAuthenticated,
+  isAdmin,
+} = require("../middlewares/protect")
 const router = express.Router()
+
+// param
+router.param("userID", getUserByID)
+router.param("genreID", getGenreByID)
+
+// create genre route
+router.post(
+  "/genre/create/:userID",
+  isLoggedin,
+  isAuthenticated,
+  isAdmin,
+  createGenre
+)
+
+// get all genres
+router.get("/genre/all", getAllGenres)
 
 module.exports = router
