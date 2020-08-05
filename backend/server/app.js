3da4091dfed8e717c7d154af6e02e1a7077a99d4
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-08-05 06:48:57 +0530

    isAdmin added middleware added

--- backend/server/app.js
+++ backend/server/app.js
@@ -8,6 +8,7 @@ const morgan = require("morgan")
 const errorHandler = require("../middlewares/error")
 const authRoutes = require("../routes/auth")
 const userRoutes = require("../routes/user")
+const genreRoutes = require("../routes/genre")
 if (process.env.NODE_ENV == "development") {
   app.use(morgan("dev"))
 }
@@ -17,6 +18,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1", userRoutes)
+app.use("/api/v1", genreRoutes)
+
 app.use(errorHandler)
 const server = app.listen(PORT, () => {
   console.log(
