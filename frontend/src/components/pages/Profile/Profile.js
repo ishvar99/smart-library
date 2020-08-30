aef5dcc735daf2cf8f27a9669dc53900811d1679
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-08-30 16:28:36 +0530

    cancel author membership and loading fixed

--- frontend/src/components/pages/Profile/Profile.js
+++ frontend/src/components/pages/Profile/Profile.js
@@ -1,22 +1,24 @@
-import React, { useEffect, useContext } from "react"
+import React, { useContext } from "react"
 import "./profile.scss"
 import { Hello } from "./components/Hello"
 import { ProfileSidebar } from "./components/ProfileSidebar"
 import { ProfileContainer } from "./components/ProfileContainer"
 import { SignUpFormAuthorCard } from "./components/SignUpFormAuthorCard"
 import { Loader } from "../../utils/Loader/Loader"
+import AuthContext from "../../../context/Auth/AuthContext"
 export const Profile = () => {
+  const { loading } = useContext(AuthContext)
   return (
     <>
-      {/* {loading ? <Loader /> : null} */}
-      <div className="Profile container my-3">
-        <div className="row">
-          <div className="col-lg-3">
+      {loading ? <Loader /> : null}
+      <div className='Profile container my-3'>
+        <div className='row'>
+          <div className='col-lg-3'>
             <Hello />
             <ProfileSidebar />
             <SignUpFormAuthorCard />
           </div>
-          <div className="col-lg-9">
+          <div className='col-lg-9'>
             <ProfileContainer />
           </div>
         </div>
--- frontend/src/components/pages/Profile/components/SignUpFormAuthorCard.js
+++ frontend/src/components/pages/Profile/components/SignUpFormAuthorCard.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react"
-import { Card, Button, Modal } from "react-bootstrap"
+import { Card, Modal } from "react-bootstrap"
 import AuthContext from "../../../../context/Auth/AuthContext"
 const originData = [
   "Asia",
@@ -23,7 +23,7 @@ export const SignUpFormAuthorCard = () => {
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
 
-  const handleSubmit = async (e) => {
+  const handleSubmitJoin = async (e) => {
     e.preventDefault()
     await updateUser(user._id, {
       role: 1,
@@ -32,6 +32,15 @@ export const SignUpFormAuthorCard = () => {
         nickname,
       },
     })
+    setShow(false)
+  }
+  const handleSubmitCancellation = async (e) => {
+    e.preventDefault()
+    await updateUser(user._id, {
+      role: 0,
+      ifAuthor: undefined,
+    })
+    setShow(false)
   }
 
   useEffect(() => {
@@ -46,107 +55,168 @@ export const SignUpFormAuthorCard = () => {
   }
 
   return (
-    user.role === 0 && (
-      <div className="SignUpFormAuthorCard mb-3">
-        <Card>
-          <Card.Body>
-            <h6>Upgrade yourself as Author</h6>
-            <button
-              className="btn btn-sm btn-outline-info"
-              onClick={handleShow}
-            >
-              Signup Now
-            </button>
-            <Modal
-              size={"lg"}
-              centered
-              show={show}
-              onHide={handleClose}
-              backdrop="static"
-              keyboard={false}
-            >
-              <div className="row px-2">
-                <div className="col-md-5 model-section1-bg"></div>
-                <div className="col-lg-7 my-auto">
-                  <Modal.Header>
-                    <h5>Signup today</h5>
-                    <h6 className="text-primary">Free 30days premium</h6>
-                  </Modal.Header>
-                  <Modal.Body>
-                    <div>
-                      <form noValidate onSubmit={handleSubmit}>
-                        <div className="form-group">
-                          <label>Name</label>
-                          <input
-                            disabled
-                            className="form-control form-control-sm"
-                            type="text"
-                            value={name}
-                          />
-                        </div>
-                        <div className="form-group">
-                          <label>Email</label>
-                          <input
-                            disabled
-                            className="form-control form-control-sm"
-                            type="email"
-                            value={email}
-                          />
-                        </div>
-                        <div className="form-group">
-                          <label>Nickname</label>
-                          <input
-                            className="form-control form-control-sm"
-                            value={nickname}
-                            onChange={(e) => setNickname(e.target.value)}
-                          />
-                        </div>
-                        <div className="form-group">
-                          <label>Origin</label>
-                          <select
-                            className="form-control form-control-sm"
-                            onChange={(e) => setOrigin(e.target.value)}
-                          >
-                            <option>--- Select ---</option>
-                            {originData.map((item, i) => {
-                              return (
-                                <option key={i} value={item}>
-                                  {item}
-                                </option>
-                              )
-                            })}
-                          </select>
-                        </div>
-                        <div className="form-group">
-                          <p className="text-muted text-small">
-                            By continuing, you agree to the Terms and Conditions
-                            of Use and Privacy Notice.
-                          </p>
-                        </div>
-                      </form>
-                    </div>
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <button
-                      className="btn btn-sm btn-secondary"
-                      onClick={handleClose}
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      className="btn btn-sm btn-primary"
-                      type="submit"
-                      onClick={handleSubmit}
-                    >
-                      Upgrade as Author
-                    </button>
-                  </Modal.Footer>
+    <>
+      {user.role === 0 && (
+        <div className='SignUpFormAuthorCard mb-3'>
+          <Card>
+            <Card.Body>
+              <h6>Upgrade yourself as Author</h6>
+              <button
+                className='btn btn-sm btn-outline-info'
+                onClick={handleShow}
+              >
+                Signup Now
+              </button>
+              <Modal
+                size={"lg"}
+                centered
+                show={show}
+                onHide={handleClose}
+                backdrop='static'
+                keyboard={false}
+              >
+                <div className='row px-2'>
+                  <div className='col-md-5 model-section1-bg'></div>
+                  <div className='col-lg-7 my-auto'>
+                    <Modal.Header>
+                      <h5>Signup today</h5>
+                      <h6 className='text-primary'>Free 30days premium</h6>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <div>
+                        <form noValidate onSubmit={handleSubmitJoin}>
+                          <div className='form-group'>
+                            <label>Name</label>
+                            <input
+                              disabled
+                              className='form-control form-control-sm'
+                              type='text'
+                              value={name}
+                            />
+                          </div>
+                          <div className='form-group'>
+                            <label>Email</label>
+                            <input
+                              disabled
+                              className='form-control form-control-sm'
+                              type='email'
+                              value={email}
+                            />
+                          </div>
+                          <div className='form-group'>
+                            <label>Nickname</label>
+                            <input
+                              className='form-control form-control-sm'
+                              value={nickname}
+                              onChange={(e) => setNickname(e.target.value)}
+                            />
+                          </div>
+                          <div className='form-group'>
+                            <label>Origin</label>
+                            <select
+                              className='form-control form-control-sm'
+                              onChange={(e) => setOrigin(e.target.value)}
+                            >
+                              <option>--- Select ---</option>
+                              {originData.map((item, i) => {
+                                return (
+                                  <option key={i} value={item}>
+                                    {item}
+                                  </option>
+                                )
+                              })}
+                            </select>
+                          </div>
+                          <div className='form-group'>
+                            <p className='text-muted text-small'>
+                              By continuing, you agree to the Terms and
+                              Conditions of Use and Privacy Notice.
+                            </p>
+                          </div>
+                        </form>
+                      </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <button
+                        className='btn btn-sm btn-secondary'
+                        onClick={handleClose}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        className='btn btn-sm btn-primary'
+                        type='submit'
+                        onClick={handleSubmitJoin}
+                      >
+                        Upgrade as Author
+                      </button>
+                    </Modal.Footer>
+                  </div>
                 </div>
-              </div>
-            </Modal>
-          </Card.Body>
-        </Card>
-      </div>
-    )
+              </Modal>
+            </Card.Body>
+          </Card>
+        </div>
+      )}
+      {user.role === 1 && (
+        <div className='SignUpFormAuthorCard mb-3'>
+          <Card>
+            <Card.Body>
+              <h6>Manage your author membership</h6>
+              <button
+                className='btn btn-sm btn-outline-info'
+                onClick={handleShow}
+              >
+                Manage
+              </button>
+              <Modal
+                size={"lg"}
+                centered
+                show={show}
+                onHide={handleClose}
+                backdrop='static'
+                keyboard={false}
+              >
+                <div className='row px-2'>
+                  <div className='col-md-5 model-section1-bg'></div>
+                  <div className='col-lg-7 my-auto'>
+                    <Modal.Header>
+                      <h5>Manage author membership</h5>
+                      <h6 className='text-primary'></h6>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <div>
+                        Do you really want to cancel your author membership?
+                      </div>
+                      <p className='text-muted text-small'>
+                        <small>
+                          By continuing, you agree to the Terms and Conditions
+                          of Use and Privacy Notice.
+                        </small>
+                      </p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <button
+                        className='btn btn-sm btn-secondary'
+                        onClick={handleClose}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        className='btn btn-sm btn-primary'
+                        type='submit'
+                        onClick={handleSubmitCancellation}
+                      >
+                        Confirm Cancellation
+                      </button>
+                    </Modal.Footer>
+                  </div>
+                </div>
+              </Modal>
+            </Card.Body>
+          </Card>
+        </div>
+      )}
+    </>
   )
 }
